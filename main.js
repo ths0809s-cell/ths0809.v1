92607ad161194589364f1f42c3c301866219c92c
ths0809s <ths0809s@gmail.com>
2026-01-31 03:09:39 +0000

    fix: 실행 오류 수정

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-깃허브 푸시 해줘//-- SETUP --------------------------------------------------------------------
+//-- SETUP --------------------------------------------------------------------
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
